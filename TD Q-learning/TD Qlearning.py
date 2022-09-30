b31fb4751d4df73752d910a0d79f747a2cd2021c
Fareha Sultan <66802211+farehasultan@users.noreply.github.com>
2022-09-30 17:53:55 -0400

    Add files via upload

---
+++ TD Q-learning/TD Qlearning.py
@@ -0,0 +1,100 @@
+#Fareha Sultan 100968491
+#Assignment 3 - Introduction to Artificial Intelligence
+
+'''
+Consider a cat and a mouse in a room with four squares, as illustrated below. If the cat and mouse occupy the
+same square, the mouse receives negative reward. If the cat and mouse occupy different squares, the mouse
+receives positive reward. In this assignment, we will use temporal difference Q-learning to learn the optimal policy
+for the mouse to achieve as much reward as possible.
+
+------------------------
+|  Square W | Square X |
+------------------------
+| Square Y |  Square Z |
+------------------------
+The mouse agent can take the following actions: do not move or move to a horizontally or vertically adjacent
+square.
+Assume that this is a fully observable environment. That is, the mouse knows for all times its position and the cat’s
+position.
+Assume that this is a discrete time environment. At each time, the mouse agent may take one action. Furthermore,
+at each time, with some probability, the cat may move to a horizontally or vertically adjacent square.
+
+'''
+import numpy
+import sys
+
+class td_qlearning:
+
+  alpha = 0.2
+  gamma = 0.9
+  qvalues={}
+
+  def __init__(self, trial_filepath):
+    trial =[]
+    # trial_filepath is the path to a file containing a trial through state space
+    squares = ['W','X','Y','Z']
+    # A grid(x,y)  of squares ['W','X','Y','Z'] where Y=(0,0), Z=(0,1), W=(1,0), X=(1,1)
+    with open(trial_filepath) as f: 
+      for line in f.readlines():
+        trial.append(line[:-1].split(','))
+        f.close()
+
+    for M in squares:
+      for C  in squares:
+        state = str(M+C) #state are represented as MC, where M=mouse's square and C=cat's square
+        if (state[0] == 'W'): 
+            # actions: up (U), down (D), left(L), right(R) and do not move (N)
+            td_qlearning.qvalues[state] = {"D": 0, "R": 0, "N": 0} 
+        if (state[0] == 'X'):
+            td_qlearning.qvalues[state] = {"D": 0, "L": 0, "N": 0}
+        if (state[0] == 'Y'):
+            td_qlearning.qvalues[state] = {"U": 0, "R": 0, "N": 0}
+        if (state[0] == 'Z'):
+            td_qlearning.qvalues[state] = {"U": 0, "L": 0, "N": 0}
+    
+    for i in range(len(trial)-1):
+        #Q(s,a)
+        state = trial[i][0]
+        action = trial[i][1]
+        q_value = td_qlearning.qvalues[state][action] 
+        #reward
+        reward = td_qlearning.reward(state)
+        #Q(s',a')
+        state_prime = trial[i+1][0]
+        action_prime = trial[i+1][1]
+        q_prime_values = td_qlearning.qvalues[state_prime].values()
+        #calling calculate 
+        updated_qvalue = td_qlearning.calculate(q_value,reward,q_prime_values)
+        #update the td_qlearning.qvalues
+        td_qlearning.qvalues[state][action] = updated_qvalue
+    
+  def calculate(q,r,m):
+      #The equation for Temporal Difference Q-learning
+      #Q(s,a) <- Q(s,a) + alpha * ( r(s) + gamma * max_a'(Q(s',a')) - Q(s,a))
+      newQ = q + td_qlearning.alpha *(r + td_qlearning.gamma * max(m) - q) 
+      return newQ
+           
+  def reward(state):
+      #Example: state = "WW"
+      if(state[0]==state[1]):
+          return -1
+      else:
+        return 1  
+  def qvalue(self, state, action):
+    # state is a string representation of a state
+    # action is a string representation of an action
+    q = td_qlearning.qvalues[state][action]
+    # Return the q-value for the state-action pair
+    return q
+
+  def policy(self, state):
+    # state is a string representation of a state
+    a = max(td_qlearning.qvalues[state], key = (td_qlearning.qvalues[state]).get)  
+    # Return the optimal action under the learned policy
+    return a
+  
+  
+  
+
+#td = td_qlearning("Inputs/Examples/Example0/trial.csv")
+
